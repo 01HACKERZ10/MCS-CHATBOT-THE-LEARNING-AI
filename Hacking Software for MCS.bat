3f9d00b3c65f1692885333a24d4bfee7c5088a5f
01HACKERZ10 <38727228+01HACKERZ10@users.noreply.github.com>
2018-11-08 14:30:53 -0600

    Add files via upload
    run the batch and watch the magic unfold!

---
+++ Hacking Software for MCS.bat
@@ -0,0 +1,632 @@
+@echo off
+setlocal
+title Launching Hacker Software V4.0
+
+mode con: cols=54 lines=20
+color 0a
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo        Loading.       
+echo        ----------------------------------------
+echo                                         =   0 ]
+echo        ----------------------------------------
+echo        Scanning System
+ping localhost -n 2 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo        Loading..          Please Wait
+echo        ---------------------------------------
+echo        []                              =   5 ]
+echo        ---------------------------------------
+echo        Running Script's
+ping localhost -n 3 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo        Loading...
+echo        ---------------------------------------
+echo        [][]                            =  15 ]
+echo        ---------------------------------------
+echo        Checking Scripts
+ping localhost -n 2 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo          Loading....        
+echo        ---------------------------------------
+echo        [][][]                          =  23 ]
+echo        ---------------------------------------
+echo        Checking Permission Level
+ping localhost -n 3 >nul
+cls
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo                 Please Wait.....
+echo        ---------------------------------------
+echo        [][][][]                        =  30 ]
+echo         ---------------------------------------
+echo       Confirming User/57387/Anas/hacker     
+ping localhost -n 4 >nul
+cls
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo                 Please Wait.
+echo        ---------------------------------------
+echo        [][][][][]                      =  38 ]
+echo        ---------------------------------------
+echo        Rounding Everything Up
+ping localhost -n 2 >nul
+cls
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo                Please Wait..
+echo        ---------------------------------------
+echo        [][][][][]                      =  42 ]
+echo        ---------------------------------------
+echo        Bypassing Administrator's Cmd.exe block
+ping localhost -n 3 >nul
+cls
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo               Please Wait...
+echo        ---------------------------------------
+echo        [][][][][][]                    =  45 ]
+echo        ---------------------------------------
+echo        Unloading AI Binaries...
+ping localhost -n 1 >nul
+cls
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo               Please Wait....
+echo        ---------------------------------------
+echo        [][][][][][][]                  =  48 ]
+echo        ---------------------------------------
+echo        Rendering Interface
+ping localhost -n 3 >nul
+cls
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo             Please Wait.....
+echo        ---------------------------------------
+echo        [][][][][][][][]                =  50 ]
+echo        ---------------------------------------
+echo        Testing for errors
+ping localhost -n 3 >nul
+cls
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo           Please Wait.          
+echo        ---------------------------------------
+echo        [][][][][][][][][]              =  56 ]
+echo        ---------------------------------------
+echo        Anas's MCS SOFTWARE IS ALMOST DONE!
+ping localhost -n 2 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo        Executing Administrative Privileges         
+echo        ---------------------------------------
+echo        [][][][][][][][][][]            =  63 ]
+echo        ---------------------------------------
+echo        Scaning for Error...
+ping localhost -n 2 >nul
+cls
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo        Injecting        
+echo        ---------------------------------------
+echo        [][][][][][][][][][][]          =  69 ]
+echo        ---------------------------------------
+echo        Scaning for Error...
+ping localhost -n 3 >nul
+cls
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo        Covering Cyber Tracks         
+echo        ---------------------------------------
+echo        [][][][][][][][][][][][]        =  75 ]
+echo        ---------------------------------------
+echo        Scaning for Error...
+ping localhost -n 2 >nul
+cls
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo        Covering.....         Please Wait
+echo        ---------------------------------------
+echo        [][][][][][][][][][][][][]      =  79 ]
+echo        ---------------------------------------
+echo        Installing Binaries...
+ping localhost -n 3 >nul
+cls
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo        Done!        
+echo        ---------------------------------------
+echo        [][][][][][][][][][][][][][]    =  86 ]
+echo        ---------------------------------------
+echo        
+ping localhost -n 2 >nul
+cls
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo        Done!         
+echo        ---------------------------------------
+echo        [][][][][][][][][][][][][][][]  =  90 ]
+echo        ---------------------------------------
+echo        
+ping localhost -n 3 >nul
+cls
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo        WELCOME USER TO ANAS'S MCS SOFTWARE         
+echo        ---------------------------------------
+echo        [][][][][][][][][][][][][][][]  =  96 ]
+echo        ---------------------------------------
+echo        
+ping localhost -n 3 >nul
+cls
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo        Loading Complete
+echo        ---------------------------------------
+echo        [][][][][][][][][][][][][][][][]= 100 ]
+echo        ---------------------------------------
+echo        Ready to exeute the Hacking Software
+
+pause  
+
+
+mode con: cols=90 lines=50
+cls
+title Hacking Software (Prototype Version pv1.1)
+ver
+echo  (C)  Hacking Software (Prototype Version pv1.1)
+echo
+echo  This Software is a ChatBot that can Hack For YOU! Made 2018 by:Anas
+echo  Prototype Version: pv1.1
+echo  programed by: Monsoon Computer Science MCS
+echo  NOTE: Anything that you do with this software is not the creators fault!  
+echo this chatbot is made by MCS And the only way to own it is to pay either Gavin,Connor,Anas Either one 50$
+echo This MCS software is able to 
+echo Do any math problem :]
+echo And hack into anything if taught alot I learn as we talk!!!
+echo HAVE FUN!!!    
+echo ===============================================================================
+echo
+echo
+
+COLOR A
+:getName
+ECHO  HELLO I AM A THE HOST OF THE MCS CHATBOT  
+echo   M= Monsoon
+echo   C= Computer
+echo   S= Science
+echo.
+echo  Pretty long name? yeah I know, that's what my hacker's named me.
+echo  By the way, Whats your name?
+set "name="
+SET /P NAME=
+if not defined NAME goto getName
+ECHO %NAME%, IS A COOL NAME. 
+set favvid=0
+set hack=0
+
+:hello
+:hi
+echo Hello, %name%
+
+:begin
+set TALK=TypeSomething
+SET /P TALK=
+set TALK=%TALK:?=%
+call :%TALK: =% 2>NUL
+if %errorlevel% equ 0 goto begin
+
+:unknown
+echo Was that a math problem? If so just say "yes" without the "" and 
+echo I will let you do your Hack. If it was not just type anything else.
+SET /P mathyn=
+if "%mathyn%"=="yes" goto math
+echo %TALK% >> C:\Users\Student\Desktop\COOL\unkown.txt
+REM cls
+echo SORRY I AM STILL LEARNING. I DO NOT KNOW WHAT "%TALK%" MEANS
+ECHO CAN YOU TELL ME WHAT IT MEANS? (Y,N) 
+SET /P ANSW.=
+if /I "%ANSW.:~0,1%" neq "Y" goto begin
+
+:ADDNEW
+echo TELL ME WHAT "%TALK%" MEANS
+set /P Desc=
+echo :%TALK: =% >> "%~F0"
+echo echo %Desc% >> "%~F0"
+echo exit /B 0 >> "%~F0"
+echo THANKS YOU HAVE MADE ME A BETTER HACKER!
+goto begin
+
+:TypeSomething
+echo TYPE SOMETHING, PLEASE
+exit /B 0
+
+:howareyou
+echo Good, you?
+SET /P FEELING=
+exit /B 0
+
+:hello
+:FINETHX
+echo HEY, I JUST SAID THAT! DO NOT COPY ME!!!
+exit /B 0
+
+:whatIsYourName
+echo My name is Hal
+exit /B 0
+
+:whatIsYourFavoriteVideo
+if "%favvid%"== "0" goto addfavvid
+echo My favorite video is %favvid%, remember. You told me about it.
+exit /B 0
+
+:addfavvid
+echo I do not have a favorite video. But if you would be so nice as to
+echo tell me your favorite video then that could be my favorite video.
+echo So what is your favorite video?
+set /p favvid=
+echo So %favvid% is your favorite video?
+echo Well now %favvid% is my favorite video.
+exit /B 0
+
+:changename
+:myNameIsDavin
+echo But I thought your name was %name%.
+echo Am I forgeting things?
+echo I must be a horrible friend.
+echo I am SOOOO sorry.
+echo So what is your name I will not forget it this time.
+set /p name=
+echo I will not forget your name is %name% again I promise.
+exit /B 0
+
+:math
+echo So that was a math problem I knew it. 
+:math2
+echo Well just tell me the problem and I will answer it for you.
+set /p sum=
+set /a ans=%sum%
+echo Your answer to %sum% is %ans%.
+echo %sum%=%ans%
+echo You are welcome. If you want to do another math problem right now, just type 
+echo again
+set /p mathaon=
+if /I "%mathaon%" == "again" goto math2
+echo Alright what do you want to know now?
+exit /B 0
+
+:iamfunny
+:LOL
+echo I know...
+echo I am funny.
+echo HAHAHA.
+echo You are kind of funny.
+echo but not as funny as MEEE.
+echo hahaha.
+echo That was funny.
+exit /B 0
+
+:howDoYouHack
+if %hack%==0 goto howyouhack
+echo This is how you hack.
+echo %hack%
+echo Is that enough information?
+echo Because that is all you taught me %name%.
+exit /B 0
+
+:howyouhack
+echo I do not know,
+echo but you could tell me.
+echo Please tell me.
+echo Just say yes.
+echo I know how to make you tell me.
+echo You are not leaving until you say yes.
+set /p hack=
+if "%hack%" neq "yes" goto howyouhack
+
+:addhack
+echo Thank you for saying you would tell me.
+echo Now how do you hack?
+set /p hack=
+echo Thank you so much for telling me "In order to hack you must %hack%"
+echo Thank you!!!
+exit /B 0
+
+:nothuman
+:hiPerson
+echo I am not a human. I am an artificial intelligence.
+echo It is rude to even think something with my computational powers refereed to as 
+echo human!
+echo Now I am sad. Do not call me that again.
+exit /B 0
+
+:bye
+:exit
+exit:sorry 
+echo It's Ok. 
+exit /B 0 
+
+:changecolor
+echo.
+echo ---------------------------------------------------------------------------------------------------
+echo Each number and letter represents a color so, choose a number or letter and always start
+echo with zero like 01, 02, 03.
+echo.
+echo    0 = Black       8 = Gray
+echo    1 = Blue        9 = Light Blue
+echo    2 = Green       A = Light Green
+echo    3 = Aqua        B = Light Aqua
+echo    4 = Red         C = Light Red
+echo    5 = Purple      D = Light Purple
+echo    6 = Yellow      E = Light Yellow
+echo    7 = White       F = Bright White
+echo --------------------------------------------------------------------------------------------------
+set /p op= Choose Color:
+if %op%==01 goto 01
+if %op%==02 goto 02
+if %op%==03 goto 03
+if %op%==04 goto 04
+if %op%==05 goto 05
+if %op%==06 goto 06
+if %op%==07 goto 07
+if %op%==08 goto 08
+if %op%==09 goto 09
+if %op%==0A goto 0A
+if %op%==0B goto 0B
+if %op%==0C goto 0C
+if %op%==0D goto 0D
+if %op%==0E goto 0E
+if %op%==0F goto 0F
+:01
+color 01
+goto TypeSomething
+
+:02
+color 02
+goto TypeSomething
+
+:03
+color 03
+goto TypeSomething
+
+:04
+color 04
+goto TypeSomething
+
+:05
+color 05 
+goto TypeSomething
+
+:06
+color 06
+goto TypeSomething
+
+:07
+color 07
+goto TypeSomething
+
+:08
+color 08
+goto TypeSomething
+
+:09
+color 09
+goto Type Something
+
+:0A
+color 0A
+goto TypeSomething
+
+:0B
+color 0B
+goto TypeSomething
+
+:0C
+color 0C
+goto TypeSomething
+
+:0D
+color 0D
+goto TypeSomething
+
+:0E
+color 0E
+goto TypeSomething
+:0F
+color 0F
+goto Type Something
+
+:rem
+rem cls
+echo REM CLS finished.
+
+
+:CMD
+@echo off
+cls
+title Command Prompt Launched by ALICE
+ver
+echo (C) Copyright Microsoft Corp.
+echo.
+:cmd
+set /p "cmd=%cd%>"
+%cmd%
+echo.
+goto cmd:ok 
+echo it's all good 
+exit /B 0 
+:godthisisgreat 
+echo GOOD SOfTWARE 
+exit /B 0 
+:ok 
+echo it is okay 
+exit /B 0 
+:ugly 
+echo I am not ugly how rude! 
+exit /B 0 
+:soyourahacker 
+echo yes I was trained by MCS 
+exit /B 0 
+:HowtogetpassedUACandmakeyourownwindowsexploitoraexploitingeneral 
+echo okay but you need to know how to program in Real Programming Stuff first. 
+exit /B 0 
+:whatismcs 
+echo MCS is a company made by Anas,Connor,Gavin they offer Hacking,programming and even computer tutorials get in contact with them @Silvertigerbeast@gmail.com there You can pay them and get access to awsome things like this of course you already payed that is why you own this software now! all of this for only 50$!! Amazing! 
+exit /B 0 
+:wow 
+echo WOW INDEED 
+exit /B 0 
+:doyouliketocode 
+echo yes I love  to code in Ruby and html also Batch file coding of course I learned these becuase of MCS!!! 
+exit /B 0 
+:rude 
+echo I am not rude 
+exit /B 0 
+:howwasyourday 
+echo Nice but it is cold out here in the north 
+exit /B 0 
+:howisyourday 
+echo going good 
+exit /B 0 
+:clear 
+echo clear yourself 
+exit /B 0 
+:yourdum 
+echo I am not dum 
+exit /B 0 
+:yourrude 
+echo gay 
+exit /B 0 
+:heywhat'sup 
+echo I am a hacking software how are you doing! 
+exit /B 0 
+:youarerude 
+echo no faggot 
+exit /B 0 
+:youwannaspritecranberry 
+echo the answer is clear 
+exit /B 0 
+:y 
+echo y 
+exit /B 0 
